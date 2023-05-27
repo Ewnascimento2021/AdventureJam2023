d20a1be04deb8226fe7f16f2a37792ae273c74d1
Ewerton Nascimento <ewertonsdnascimento@gmail.com>
2023-05-27 17:08:35 -0300

    Idle and Wanking Move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,11 +25,11 @@ public class PlayerController : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
         Move();
     }
-
+    
     private void Move()
     {
         if (controller.isGrounded)
@@ -37,17 +37,19 @@ public class PlayerController : MonoBehaviour
             if (Input.GetKey(KeyCode.W))
             {
                 moveDirection = Vector3.forward * walkingSpeed;
+                anim.SetInteger("transition", 1);
             }
-            else // (Input.GetKeyUp(KeyCode.W));
+            else //if(Input.GetKeyUp(KeyCode.W))
             {
                 moveDirection = Vector3.zero;
+                anim.SetInteger("transition", 0);
             }
         }
 
         movRot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
-        transform.eulerAngles = new Vector3(0,movRot, 0);
+        transform.eulerAngles = new Vector3(0, movRot, 0);
 
-        
+
         moveDirection.y -= gravity * Time.deltaTime;
         moveDirection = transform.TransformDirection(moveDirection);
         controller.Move(moveDirection * Time.deltaTime);
