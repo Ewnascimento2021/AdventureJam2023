229c0c5c6915c22f9b45eea8867039d4db5c603c
Ewerton Nascimento <ewertonsdnascimento@gmail.com>
2023-05-30 15:42:23 -0300

    Correct Sensibility2.1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,25 +10,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float walkingSpeed;
     [SerializeField]
+    private float rotateSpeed;
+    [SerializeField]
     private float gravity;
-    [SerializeField]
-    private float rotSpeed;
-    [SerializeField]
-    private Vector2 turn;
     [SerializeField]
     private float sensitivity;
     [SerializeField]
-    private Vector3 deltaMove;
+    private Vector2 turn;
     [SerializeField]
-    private float rotateSpeed;
-
-
-    private float movRot;
+    private Vector3 deltaMove;
+    
 
     private CharacterController cc;
     private Animator anim;
     private Vector3 moveDirection;
-
 
     state state_;
     enum state
@@ -55,19 +50,10 @@ public class PlayerController : MonoBehaviour
         turn.x += Input.GetAxis("Mouse X") * sensitivity;
         turn.y += Input.GetAxis("Mouse Y") * sensitivity;
         cc.transform.localRotation = Quaternion.Euler(0, turn.x, 0);
-        // transform.localRotation = Quaternion.Euler(-turn.x, 0, 0);
 
         deltaMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * rotateSpeed * Time.deltaTime;
         cc.transform.Translate(deltaMove);
 
-        //if (turn.x > 0)
-        //{
-        //    anim.SetBool("RightSide", true);
-        //}
-        //else if (turn.x < 0)
-        //{
-        //    anim.SetBool("LeftSide", true);
-        //}
     }
     private void handleInput()
     {
@@ -125,80 +111,8 @@ public class PlayerController : MonoBehaviour
                 }
                 break;
         }
-
-        //movRot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
-        //transform.eulerAngles = new Vector3(0, movRot, 0);
         moveDirection.y -= gravity * Time.deltaTime;
         moveDirection = transform.TransformDirection(moveDirection);
         cc.Move(moveDirection * Time.deltaTime);
-
-        //moveDirection.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        //cc.Move(moveDirection * walkingSpeed * Time.deltaTime);
-        //cc.Move(Vector3.down * Time.deltaTime);
-
     }
-
-
 }
-//private void Move()
-
-//{
-//    if (cc.isGrounded)
-//    {
-//        if (Input.GetKey(KeyCode.W))
-//        {
-//            moveDirection = Vector3.forward * walkingSpeed;
-//            anim.SetInteger("transition", 1);
-//        }
-//        else if (Input.GetKey(KeyCode.S))
-//        {
-//            moveDirection = Vector3.forward * walkingSpeed * -1;
-//            anim.SetInteger("transition", 1);
-//        }
-//        else //if(Input.GetKeyUp(KeyCode.W))
-//        {
-//            moveDirection = Vector3.zero;
-//            anim.SetInteger("transition", 0);
-//        }
-//    }
-
-
-
-
-//    //Ataque
-//    if (Input.GetKey(KeyCode.Q))
-//    {
-//        anim.SetInteger("transition", 2);
-//    }
-//    //Defende
-//    if (Input.GetKey(KeyCode.E))
-//    {
-//        anim.SetInteger("transition", 3);
-//    }
-//    //Anda para Trás
-//    if (Input.GetKey(KeyCode.S))
-//    {
-//        anim.SetInteger("transition", 4);
-//    }
-//    //Pulo
-//    if (Input.GetKey(KeyCode.Space))
-//    {
-//        anim.SetInteger("transition", 5);
-//    }
-
-
-
-
-
-//    
-
-
-//    if (moveDirection != Vector3.zero)
-//    {
-//        anim.SetInteger("transition", 1);
-//        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * slerp);
-//    }
-//    else
-//    {
-//        anim.SetInteger("transition", 0);
-//    }
